e3f69fe6738d0f43e179425a35be4db8a779a159
KOITT <KOITT@KOITT-PC03>
2018-02-26 09:46:54 +0900

    spring 17번 예제

--- Spring17BoardLogin/src/com/koitt/board/controller/IndexWebController.java
+++ Spring17BoardLogin/src/com/koitt/board/controller/IndexWebController.java
@@ -1,13 +1,11 @@
 package com.koitt.board.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.koitt.board.service.UsersService;
+
 
 @Controller
 public class IndexWebController {

49b64139007bb15786d97b53524ffd6f43e18467
KOITT <KOITT@KOITT-PC03>
2018-02-27 15:56:52 +0900

    JUnit 수정

--- SpringJUnit08LearnningTest02/src/com/koitt/junit/JunitTest.java
+++ SpringJUnit08LearnningTest02/src/com/koitt/junit/JunitTest.java
@@ -6,9 +6,6 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;

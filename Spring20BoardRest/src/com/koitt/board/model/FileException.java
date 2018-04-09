6e8680717f431acf4f26d8ba3de38e6c50a501b8
KOITT <KOITT@KOITT-PC03>
2018-04-09 15:31:18 +0900

    취소

--- Spring20BoardRest/src/com/koitt/board/model/FileException.java
+++
@@ -1,9 +0,0 @@
-package com.koitt.board.model;
-
-public class FileException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public FileException(String msg) {
-		super(msg);
-	}
-}

b377806edb42adb53ce1cf42ac9f44b1a8f00a70
KOITT <KOITT@KOITT-PC03>
2018-04-09 14:18:37 +0900

    Spring 20 Board Rest

---
+++ Spring20BoardRest/src/com/koitt/board/dao/AuthorityDao.java
@@ -0,0 +1,10 @@
+package com.koitt.board.dao;
+
+import com.koitt.board.model.Authority;
+import com.koitt.board.model.UsersException;
+
+public interface AuthorityDao {
+	
+	public Authority select(Integer id) throws UsersException;
+
+}
---
+++ Spring20BoardRest/src/com/koitt/board/model/FileException.java
@@ -0,0 +1,9 @@
+package com.koitt.board.model;
+
+public class FileException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+}
